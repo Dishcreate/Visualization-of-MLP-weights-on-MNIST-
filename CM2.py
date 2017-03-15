f277a188e8c921009860aa990c112ec0d0d4fd92
DInusha DIlanka <dinushaeng@gmail.com>
2017-03-16 01:26:34 +0530

    Add files via upload

---
+++ CM2.py
@@ -0,0 +1,37 @@
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+from sklearn.neural_network import MLPClassifier
+from sklearn.cross_validation import train_test_split
+df=pd.read_csv('features.csv')
+df1=pd.read_csv('labels.csv')
+X=df.fillna(df)
+y=df1.fillna(df1)
+
+
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=45)
+
+
+# mlp = MLPClassifier(hidden_layer_sizes=(100, 100), max_iter=400, alpha=1e-4,
+#                     solver='sgd', verbose=10, tol=1e-4, random_state=1)
+mlp = MLPClassifier(hidden_layer_sizes=(50,), max_iter=10, alpha=1e-4,
+                    solver='sgd', verbose=10, tol=1e-4, random_state=1,
+                    learning_rate_init=.1)
+
+mlp.fit(X_train, y_train)
+print("Training set score: %f" % mlp.score(X_train, y_train))
+print("Test set score: %f" % mlp.score(X_test, y_test))
+
+fig, axes = plt.subplots(4, 4)
+# use global min / max to ensure all weights are shown on the same scale
+vmin, vmax = mlp.coefs_[0].min(), mlp.coefs_[0].max()
+for coef, ax in zip(mlp.coefs_[0].T, axes.ravel()):
+    ax.matshow(coef.reshape(1,1), cmap=plt.cm.gray, vmin=.5 * vmin,
+               vmax=.5 * vmax)
+    ax.set_xticks(())
+    ax.set_yticks(())
+
+plt.show()
